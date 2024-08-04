fc286bc500784558a9e65f257af44999d42e6737
Ray Toal <rtoal@lmu.edu>
2024-08-04 13:04:50 -0700

    TypeScript

--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -1,1 +1,12 @@
-// Write your solutions to the exercises in this file.
+export function change(amount: number): number[] {
+  if (amount < 0) {
+    throw new RangeError("amount cannot be negative")
+  }
+  let [coins, remaining] = [[] as number[], amount]
+  for (let denomination of [25, 10, 5]) {
+    coins.push(Math.floor(remaining / denomination))
+    remaining %= denomination
+  }
+  coins.push(remaining)
+  return coins
+}
