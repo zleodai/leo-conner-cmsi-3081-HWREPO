0c314059507690c5d09c23dd783e02d2450fc5fe
Ray Toal <rtoal@lmu.edu>
2024-08-11 17:43:59 -0700

    Better Python change implementation

--- python/exercises.py
+++ python/exercises.py
@@ -7,17 +7,22 @@ def change(amount: int) -> dict[int, int]:
         raise TypeError('Amount must be an integer')
     if amount < 0:
         raise ValueError('Amount cannot be negative')
-    counts = {}
+    counts, remaining = {}, amount
     for denomination in (25, 10, 5, 1):
-        counts[denomination], amount = divmod(amount, denomination)
+        counts[denomination], remaining = divmod(remaining, denomination)
     return counts
+
 
 # Write your first then lower case function here
 
+
 # Write your powers generator here
+
 
 # Write your say function here
 
+
 # Write your line count function here
+
 
 # Write your Quaternion class here
