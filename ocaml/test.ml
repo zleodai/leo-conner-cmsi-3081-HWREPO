3296611124e1e2077f9dc457b3be257310836a67
Ray Toal <rtoal@lmu.edu>
2024-08-14 08:02:12 -0700

    Tests for OCaml first_then_apply

--- ocaml/test.ml
+++ ocaml/test.ml
@@ -1,15 +1,46 @@
 open Exercises;;
 
-Printf.printf "Testing change()\n";;
+(* -------------------------------------------------------------------------- *)
+let passes = ref 0;;
+let failures = ref 0;;
 
-assert (change 0 = [0; 0; 0; 0]);;
+let suite name = Printf.printf "\nTesting %s" name;;
+
+let expect condition =
+  if condition then
+    (Printf.printf " .";
+    passes := !passes + 1)
+  else
+    (Printf.printf " F";
+    failures := !failures + 1);;
+ (* -------------------------------------------------------------------------- *)
+
+suite "change";;
 match change (-50) with
-  | exception Negative_Amount -> assert true
-  | result -> assert false;;
-assert (change(1) = [0; 0; 0; 1]);;
-assert (change(99) = [3; 2; 0; 4]);;
-assert (change(42) = [1; 1; 1; 2]);;
-assert (change(100000000037) = [4000000001; 1; 0; 2]);;
-assert (change(10000000000005) = [400000000000; 0; 1; 0]);;
+  | exception Negative_Amount -> expect true
+  | result -> expect false;;
+expect (change 0 = [0; 0; 0; 0]);;
+expect (change(1) = [0; 0; 0; 1]);;
+expect (change(42) = [1; 1; 1; 2]);;
+expect (change(99) = [3; 2; 0; 4]);;
+expect (change(100000000037) = [4000000001; 1; 0; 2]);;
+expect (change(10000000000005) = [400000000000; 0; 1; 0]);;
 
-Printf.printf "All tests pass\n"
+(* Uncomment the following tests as you complete the exercises *)
+
+(*
+let non_empty s = s <> "";;
+let length_greater_than_3 s = String.length s > 3;;
+let lower s = Some (String.lowercase_ascii s);;
+let square n = Some(n * n);;
+
+suite "first_then_apply";;
+expect (first_then_apply [] non_empty lower = None);;
+expect (first_then_apply [""; "A"; "B"] non_empty lower = Some "a");;
+expect (first_then_apply [""; "A"; "ABC"] length_greater_than_3 lower = None);;
+expect (first_then_apply ["ABC"; "ABCD"; "ABCDE"] length_greater_than_3 lower = Some "abcd");;
+expect (first_then_apply [1; 2; 3] (fun n -> n > 1) square = Some 4);;
+expect (first_then_apply [1; 2; 3] (fun n -> n > 3) square = None);;
+*)
+
+Printf.printf "\n%d passed, %d failed\n" !passes !failures;;
