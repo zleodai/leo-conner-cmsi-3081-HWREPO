5ef6008e2514d23b74b987cf7935fd6aef502b62
Ray Toal <rtoal@lmu.edu>
2024-08-25 16:52:43 -0700

    Go exercise stub

---
+++ go/restaurant.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+// A little utility that simulates performing a task for a random duration.
+// For example, calling do(10, "Remy", "is cooking") will compute a random
+// number of milliseconds between 5000 and 10000, log "Remy is cooking",
+// and sleep the current goroutine for that much time.
+
+func do(seconds int, action ...any) {
+    log.Println(action...)
+    randomMillis := 500 * seconds + rand.Intn(500 * seconds)
+    time.Sleep(time.Duration(randomMillis) * time.Millisecond)
+}
+
+// Implement the rest of the simulation here. You may need to add more imports
+// above.
