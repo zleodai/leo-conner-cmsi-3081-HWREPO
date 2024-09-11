54e4766e0b391ccef5819f9728e2ca01e85bcbdd
zleodai <awsomeleodai@gmail.com>
2024-09-10 17:23:08 -0700

    lua big part almost done

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -14,32 +14,97 @@ function change(amount)
 end
 
 -- Write your first then lower case function here
-function return_lower_case(inputString)
-  if string.len(inputString) ~= 0 then return string.lower(inputString) end
-  return nil
-end 
-
 function first_then_lower_case(stringTable, filterFunction)
   for index, value in ipairs(stringTable) do
-    if filterFunction(value) then return return_lower_case(value) end      
+    if filterFunction(value) then 
+      if string.len(value) ~= 0 then return string.lower(value) end
+      return nil
+    end      
   end
   return nil
 end
 
 -- Write your powers generator here
 function powers_generator(power, max)
-  limit = math.floor(math.log(max)/math.log(power))
-  powerGenerator = coroutine.create(function ()
+  local limit = math.floor(math.log(max)/math.log(power))
+  local powerGenerator = coroutine.create(function ()
     for i=0,limit do 
       coroutine.yield(power^i)
     end
-  end
-  )
+  end)
   return powerGenerator
 end
 
 -- Write your say function here
+function say(inputString)
+  if inputString == nil then return "" end
+  return (function(otherInputString)
+    if otherInputString == nil then return inputString end
+    return say(string.format("%s %s", inputString, otherInputString))
+  end)
+end
 
 -- Write your line count function here
+function meaningful_line_count(filePath)
+  local file = io.open(filePath, "r")
+  if file == nil then error("No such file") end
+  local lineCount = 0
+  for line in file:lines() do
+    if string.len(line) ~= 0 then
+      local firstChar = ""
+      local firstCharFound = false
+      for charIndex = 1,#line do
+        -- Im gonna assume "	" is a whitespace even doe it is a hidden character
+        if string.sub(line, charIndex, charIndex) ~= " " and string.sub(line, charIndex, charIndex) ~= "	" and not firstCharFound then 
+          firstChar = string.sub(line, charIndex, charIndex) 
+          firstCharFound = true
+        end
+      end
+      if firstChar ~= "#" and firstCharFound then lineCount = lineCount + 1 end
+    end
+  end
+  return lineCount
+end
 
 -- Write your Quaternion table here
+Quaternion = {a = 0, b = 0, c = 0, d = 0}
+function Quaternion.new(_a, _b, _c, _d)
+  local quaternion = {}
+  setmetatable(quaternion, Quaternion)
+  quaternion.a = _a
+  quaternion.b = _b
+  quaternion.c = _c
+  quaternion.d = _d
+
+  return quaternion
+end
+
+function Quaternion:__tostring()
+  local quatString = ""
+
+  if self.a ~= 0 then quatString = string.format("%d", self.a) end
+
+  if self.b == 1 and string.len(quatString) == 0 then quatString = string.format("%si", quatString)
+  elseif self.b == 1 then quatString = string.format("%s+i", quatString)
+  elseif self.b == -1 then quatString = string.format("%s-i", quatString)
+  elseif self.b > 0 then quatString = string.format("%s+%di", quatString, self.b)
+  elseif self.b < 0 then quatString = string.format("%s-%di", quatString, self.b) end
+  
+  if self.c == 1 and string.len(quatString) == 0 then quatString = string.format("%sj", quatString)
+  elseif self.c == 1 then quatString = string.format("%s+j", quatString)
+  elseif self.c == -1 then quatString = string.format("%s-j", quatString)
+  elseif self.c > 0 then quatString = string.format("%s+%dj", quatString, self.c)
+  elseif self.c < 0 then quatString = string.format("%s-%dj", quatString, self.c) end
+
+  if self.d == 1 and string.len(quatString) == 0 then quatString = string.format("%sk", quatString)
+  elseif self.d == 1 then quatString = string.format("%s+k", quatString)
+  elseif self.d == -1 then quatString = string.format("%s-k", quatString)
+  elseif self.d > 0 then quatString = string.format("%s+%dk", quatString, self.d)
+  elseif self.d < 0 then quatString = string.format("%s-%dk", quatString, self.d) end
+  
+  if string.len(quatString) == 0 then quatString = "0" end
+
+  print(string.format("\nFormatted Quat: %s", quatString))
+
+  return quatString
+end
