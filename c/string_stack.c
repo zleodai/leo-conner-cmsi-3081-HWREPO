f93168f5ac84caad30252f8565c84bc82eaf66e9
zleodai <awsomeleodai@gmail.com>
2024-11-21 13:52:07 -0800

    c stuff

--- c/string_stack.c
+++ c/string_stack.c
@@ -3,5 +3,24 @@
 
 struct _Stack {
     char *internalArray[];
-    
+    int *maxSize;
 };
+
+stack_response create() {
+    stack_response stackResponse;
+    
+    int *maxSizePtr = (int*)malloc(sizeof(int));
+    *maxSizePtr = 16;
+
+    char *internalArrayPtr[] = (char*)malloc(*maxSizePtr * sizeof(char*));
+    
+    _Stack *newStack = (_Stack*)malloc(sizeof(*maxSizePtr) + sizeof(*internalArrayPtr));
+    
+    newStack->internalArray = internalArrayPtr;
+    newStack->maxSize = maxSizePtr;
+    stackResponse.stack = newStack;
+
+    response_code responseCode = success;
+    stackResponse.code = responseCode;
+    return stackResponse;
+}
